e59441d038f59ef750fb73a00f1ce95618706a41
Peter Chapman <pchapman@ptc.com>
2020-03-11 17:51:28 -0700

    Integrating Utopia for typography.

--- src/site/css/styles.11ty.js
+++ src/site/css/styles.11ty.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const postcss = require('postcss');
+const fs = require('fs')
+const path = require('path')
+const postcss = require('postcss')
 
 // the file name as an entry point for postcss compilation
 // also used to define the output filename in our output /css folder.
-const fileName = "styles.css";
+const fileName = 'styles.css'
 
 module.exports = class {
   async data () {
-    const rawFilepath = path.join(__dirname, `../_includes/postcss/${fileName}`);
+    const rawFilepath = path.join(__dirname, `../_includes/postcss/${fileName}`)
     return {
       permalink: `css/${fileName}`,
       rawFilepath,
       rawCss: await fs.readFileSync(rawFilepath)
-    };
+    }
   };
 
   async render ({ rawCss, rawFilepath }) {
@@ -25,7 +25,7 @@ module.exports = class {
       require('postcss-color-mix'),
       require('cssnano')
     ])
-    .process(rawCss, { from: rawFilepath })
-    .then(result => result.css);
+      .process(rawCss, { from: rawFilepath })
+      .then(result => result.css)
   };
 }
